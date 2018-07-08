ee03a880c0c366a8578ec995db90706a82337f4d
Praveen <praveengk401@gmail.com>
2018-07-08 16:58:01 +0530

    First Draft

---
+++ src/test/java/com/techielogic/loggerexample/LoggerexampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.techielogic.loggerexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoggerexampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
